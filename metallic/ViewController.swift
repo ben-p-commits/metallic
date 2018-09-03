a2b669259c88eb22c04feec60e02064d65e8572e
Benjamin Palmer <ben.palmer@playmakercrm.com>
2018-09-03 10:50:58 -0400

    Initial Commit

---
+++ metallic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  metallic
+//
+//  Created by Ben Palmer on 9/3/18.
+//  Copyright © 2018 Ben Palmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
